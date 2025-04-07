8e093e53ee47ed1f70156835ab99c5bcddd0f969
Alessandro Cioffi <alessandrocioffi007@gmail.com>
2025-04-07 19:52:43 +0200

    Create valori.py

---
+++ Project/valori.py
@@ -0,0 +1,23 @@
+# valori.py
+def val_domains(domains):
+    domains[(0, 0)] = {8}
+    domains[(1, 2)] = {3}
+    domains[(1, 3)] = {6}
+    domains[(2, 1)] = {7}
+    domains[(2, 4)] = {9}
+    domains[(2, 6)] = {2}
+    domains[(3, 1)] = {5}
+    domains[(3, 5)] = {7}
+    domains[(4, 4)] = {4}
+    domains[(4, 5)] = {5}
+    domains[(4, 6)] = {7}
+    domains[(5, 3)] = {1}
+    domains[(5, 7)] = {3}
+    domains[(6, 2)] = {1}
+    domains[(6, 7)] = {6}
+    domains[(6, 8)] = {8}
+    domains[(7, 2)] = {8}
+    domains[(7, 3)] = {5}
+    domains[(7, 7)] = {1}
+    domains[(8, 1)] = {9}
+    domains[(8, 6)] = {4}
